39b06c607c31469b97cd6a95b8501de214932e5b
Victor Elias <svvictorelias@gmail.com>
2022-11-03 20:39:38 -0300

    Feat: specifications typeorm

--- rentx/src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ rentx/src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -8,7 +8,7 @@ class CreateSpecificationController {
       CreateSpecificationUseCase
     );
 
-    createSpecificationUseCase.execute({ name, description });
+    await createSpecificationUseCase.execute({ name, description });
     return response.status(201).send();
   }
 }
