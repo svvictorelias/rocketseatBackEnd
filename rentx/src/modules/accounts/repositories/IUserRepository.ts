f23c19ea2c3890f2a56d147297db0c0d058011bb
Victor Elias <svvictorelias@gmail.com>
2022-11-06 11:35:24 -0300

    Feat: includes jwt

--- rentx/src/modules/accounts/repositories/IUserRepository.ts
+++ rentx/src/modules/accounts/repositories/IUserRepository.ts
@@ -1,9 +1,10 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
 import { User } from "../entities/User";
 
-interface IUserRepository {
+interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
   findByEmail(email: string): Promise<User>;
+  findById(id: string): Promise<User>;
 }
 
-export { IUserRepository };
+export { IUsersRepository };
