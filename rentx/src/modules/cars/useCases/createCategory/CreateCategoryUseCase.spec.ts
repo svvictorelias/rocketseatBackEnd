d0e6f819b6f6900164f933fc434bf23b8233df12
Victor Elias <svvictorelias@gmail.com>
2022-11-12 20:21:48 -0300

    Feat: using jest to test

---
+++ rentx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,46 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let createCategoryUsecase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("Create Category", () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUsecase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    );
+  });
+  it("should be able to create a new category", async () => {
+    const category = {
+      name: "Category test",
+      description: "Category description test"
+    };
+    await createCategoryUsecase.execute({
+      name: category.name,
+      description: category.description
+    });
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+    expect(categoryCreated).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new category with name exists", async () => {
+    expect(async () => {
+      const category = {
+        name: "Category test",
+        description: "Category description test"
+      };
+      await createCategoryUsecase.execute({
+        name: category.name,
+        description: category.description
+      });
+      await createCategoryUsecase.execute({
+        name: category.name,
+        description: category.description
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
