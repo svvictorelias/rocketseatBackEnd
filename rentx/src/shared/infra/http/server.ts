30929d6408b2a336ed400304aaa8503deb2057f1
Victor Elias <svvictorelias@gmail.com>
2022-11-16 20:43:03 -0300

    Create seed to admin

--- rentx/src/shared/infra/http/server.ts
+++ rentx/src/shared/infra/http/server.ts
@@ -6,9 +6,11 @@ import { router } from "./routes";
 
 import swaggerFile from "../../../swagger.json";
 
-import "@shared/infra/typeorm";
+import createConnection from "@shared/infra/typeorm";
 import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
+
+createConnection();
 
 const app = express();
 app.use(express.json());
