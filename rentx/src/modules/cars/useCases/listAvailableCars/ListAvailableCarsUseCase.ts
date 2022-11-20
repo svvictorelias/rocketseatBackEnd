392ed723d85063b31d74da5e26efcb71e9d3bf87
Victor Elias <svvictorelias@gmail.com>
2022-11-20 18:26:55 -0300

    Feat: create list by brand, name and category id to car

--- rentx/src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
+++ rentx/src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
@@ -1,14 +1,18 @@
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { inject, injectable } from "tsyringe";
 
 interface IRequest {
   category_id?: string;
   brand?: string;
   name?: string;
 }
-
-class ListCarsUseCase {
-  constructor(private carsRepository: ICarsRepository) {}
+@injectable()
+class ListAvailableCarsUseCase {
+  constructor(
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
+  ) {}
   async execute({ category_id, brand, name }: IRequest): Promise<Car[]> {
     const cars = await this.carsRepository.findAvailable(
       brand,
@@ -19,4 +23,4 @@ class ListCarsUseCase {
   }
 }
 
-export { ListCarsUseCase };
+export { ListAvailableCarsUseCase };
