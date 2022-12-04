f6038eac9943a9888abc554bfa70401c49538c80
Victor Elias <svvictorelias@gmail.com>
2022-12-03 21:56:23 -0300

    feat: create integration test to list category

--- rentx/src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ rentx/src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -5,7 +5,7 @@ import createConnection from "@shared/infra/typeorm";
 import { Connection } from "typeorm";
 import { hash } from "bcryptjs";
 let connection: Connection;
-describe("Create Category Controller", () => {
+describe("List Category", () => {
   beforeAll(async () => {
     connection = await createConnection();
     await connection.runMigrations();
@@ -20,13 +20,13 @@ describe("Create Category Controller", () => {
     await connection.dropDatabase();
     await connection.close();
   });
-  it("should be able to create a new category", async () => {
+  it("should be able to list all categories", async () => {
     const responseToken = await request(app).post("/sessions").send({
       email: "admin@mail.com",
       password: "admin"
     });
     const { token } = responseToken.body;
-    const response = await request(app)
+    await request(app)
       .post("/categories")
       .send({
         name: "Category teste",
@@ -35,24 +35,12 @@ describe("Create Category Controller", () => {
       .set({
         Authorization: `Bearer ${token}`
       });
-    expect(response.status).toBe(201);
-  });
 
-  it("should not be able to create a new category if already exists", async () => {
-    const responseToken = await request(app).post("/sessions").send({
-      email: "admin@mail.com",
-      password: "admin"
-    });
-    const { token } = responseToken.body;
-    const response = await request(app)
-      .post("/categories")
-      .send({
-        name: "Category teste",
-        description: "Description teste"
-      })
-      .set({
-        Authorization: `Bearer ${token}`
-      });
-    expect(response.status).toBe(400);
+    const response = await request(app).get("/categories");
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0]).toHaveProperty("id");
+    expect(response.body[0].name).toEqual("Category teste");
   });
 });
